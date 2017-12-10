08bd673043edebfff403d23d617e523fff55c7c8
vliakop <sdi1100097@di.uoa.gr>
2017-12-10 18:48:49 +0200

    Exceptions added
    Added two exception classes: BuildError and CheckError.
    The first one will be used in the BuildVisitor.
    The latter one will be in the CheckVisitor: during type checking

---
+++ tcpackage/BuildError.java
@@ -0,0 +1,8 @@
+package tcpackage;
+
+public class BuildError extends Exception{
+	
+	public String getMessage(){
+		return "Build Error";
+	}
+}
---
+++ tcpackage/CheckError.java
@@ -0,0 +1,8 @@
+package tcpackage;
+
+public class CheckError extends Exception{
+
+	public String getMessage(){
+		return "Check Error";
+	}
+}
