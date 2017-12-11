18c43918b845b0dd5a116c10c4ea1a661fc11044
Vasileios Liakopoulos <viakop>
2017-12-11 11:43:01 +0200

    Added a TestFile

---
+++ SomeTest.java
@@ -0,0 +1,17 @@
+class SomeTest{
+	public static void main(String[] args){
+		int i = 0;
+		System.out.println(i);
+	}
+	
+	class Test{
+		int i;
+		int j;
+		
+		public int someMethod(int k){
+			int m = 69;
+			return m;
+		}
+	}
+
+}
